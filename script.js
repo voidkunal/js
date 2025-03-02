12f04d8a5a92d96db20116d29530ff69929491e0
voidkunal <voidkunal@Kunals-MacBook-Air.local>
2025-03-02 20:11:11 +0530

    avg calculate

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 // let age = 23;
 // age +2;
 // Solution :
-let age = 23;
-age + 2;
-console.log(age); 
+// let age = 23;
+// age + 2;
+// console.log(age); 
+
+// what is the value of avg after this code runs ?
+
+let hindi = 80;
+let english = 90;
+let maths = 96;
+let avg = (hindi + english + maths) / 3;
+console.log(avg);
