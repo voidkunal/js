a789ca8fc2eb1e8e816b0c3fd34bcae6b09c1f18
voidkunal <voidkunal@Kunals-MacBook-Air.local>
2025-03-02 20:08:33 +0530

    let ketword

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+// console.log('Hello, World!');
+
+// what is the value of age after this code runs ?
+// let age = 23;
+// age +2;
+// Solution :
+let age = 23;
+age + 2;
+console.log(age); 
