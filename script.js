b71815ed5a247bf435c8dc1b87a733ce30144184
voidkunal <voidkunal@Kunals-MacBook-Air.local>
2025-03-02 20:14:50 +0530

    unary operator

--- script.js
+++ script.js
@@ -10,8 +10,16 @@
 
 // what is the value of avg after this code runs ?
 
-let hindi = 80;
-let english = 90;
-let maths = 96;
-let avg = (hindi + english + maths) / 3;
-console.log(avg);
+// let hindi = 80;
+// let english = 90;
+// let maths = 96;
+// let avg = (hindi + english + maths) / 3;
+// console.log(avg);
+
+// what is the value of each variable in each line of code ?
+
+let num = 5;
+let newNum = num++;
+newNum == ++num;
+console.log(newNum);
+console.log(num);
