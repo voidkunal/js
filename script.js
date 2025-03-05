5ab41d9ac9f674a3cff2da47cf5c4ccc51c89fad
voidkunal <voidkunal@Kunals-MacBook-Air.local>
2025-03-05 17:25:36 +0530

    alter & prompt

--- script.js
+++ script.js
@@ -187,30 +187,35 @@
 
 // use switch statement to print the day week using a number variable 'day' with value 1 to 7.
 
-let day = 5;
+// let day = 5;
 
-switch(day){
-    case 1 :
-    console.log("Sunday");
-    break;
-    case 2 :
-    console.log("Monday");  
-    break;
-    case 3 :
-    console.log("Tuesday");
-    break;
-    case 4 :
-    console.log("Wednesday");
-    break;
-    case 5 :
-    console.log("Thursday");
-    break;
-    case 6 :
-    console.log("Friday");
-    break;
-    case 7 :
-    console.log("Saturday");
-    break;
-    default:
-        console.log("Choose the correct day");
-}
+// switch(day){
+//     case 1 :
+//     console.log("Sunday");
+//     break;
+//     case 2 :
+//     console.log("Monday");  
+//     break;
+//     case 3 :
+//     console.log("Tuesday");
+//     break;
+//     case 4 :
+//     console.log("Wednesday");
+//     break;
+//     case 5 :
+//     console.log("Thursday");
+//     break;
+//     case 6 :
+//     console.log("Friday");
+//     break;
+//     case 7 :
+//     console.log("Saturday");
+//     break;
+//     default:
+//         console.log("Choose the correct day");
+// }
+
+// ALter & Prompt
+
+prompt("What is your name ? : ");
+alert("Hello, World!");
